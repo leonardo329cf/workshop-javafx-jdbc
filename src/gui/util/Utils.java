267a65930e97856431b513a7e142cc7b807bc07b
Leonardo Cardozo Ferreira <leonardo329cf@gmail.com>
2019-12-19 22:29:17 -0300

    Cleaning unused imports

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -11,7 +11,6 @@ import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.util.StringConverter;
 
